d7d240afea5feedb183d5d928d5b00918681f774
Sasha <brdgn@bk.ru>
2022-09-18 11:04:52 +0300

    first commit

---
+++ hw_34/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве. [345, 897, 568, 234] -> 2
+
+Console.Write("Предложите количество элементов в массиве: ");
+int count = int.Parse(Console.ReadLine()!);
+
+// Задаём массив, заполненный случайными положительными трёхзначными числами
+int[] array = new int[count];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+// ПОКАЖЕМ ТЕПЕРЬ КОЛИЧЕСТВО ЧЁТНЫХ ЧИСЕЛ В МАССИВЕ
+int EvenCount = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        EvenCount++;
+    }
+}
+Console.WriteLine($"Количество четных элементов в массиве {EvenCount}");
+Console.WriteLine();
---
+++ hw_38/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементами массива. [3 7 22 2 78] -> 76
+
+Console.Write("Предложите количество элементов в массиве: ");
+int count = int.Parse(Console.ReadLine()!);
+Double [] array = new Double[count];
+
+Double MaxEl = array[0];
+Double MinEl = array[0];
+
+for (int i = 0; i < array.Length; i++) // Задаём массив, заполненный случайными вещественными числами числами
+{
+    array[i] = Math.Round(new Random().NextDouble() * 100, 3, MidpointRounding.AwayFromZero);
+    // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1, поэтому добавляем * 100
+}
+
+// Найдем максимальный и минимальный элемент массива (семинары по блок-схемам)
+foreach (var value in array)
+{
+    if (value < MinEl)
+    {
+        MinEl = value;
+    }
+    if (value > MaxEl)
+    {
+        MaxEl = value;
+    }
+}
+
+Console.WriteLine($"[{string.Join(" | ", array)}]"); // другой разделитель, т.к. запятых и так много
+Console.WriteLine($"Максимальный элемент в массиве - это {MaxEl}");
+Console.WriteLine($"Минимальный элемент в массиве - это {MinEl}");
+
+// Найдем разницу между максимальным и минимальным элементами
+Console.WriteLine($"Разница между этими элементами {MaxEl} - {MinEl} = {MaxEl - MinEl}");
+
+/* Набрал для пробы из книги Флёнова "Библия C#"
+int [] array = { 10, 20, 4, 10, 44, 95, 74, 28, 84, 79 };
+
+int max = array[0];
+int min = array[0];
+
+foreach (var value in array)
+{
+    if (value < min)
+        min = value;
+    if (value > max)
+        max = value;
+}
+Console.WriteLine($"[{string.Join(", ", array)}], min = {min}, max = {max}");
+*/
